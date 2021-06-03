8163078376d14d17318f7ac9eb3db50c0b418b7d
nailson13b <nailson.oliveira@outlook.com>
2021-06-03 17:26:30 -0300

    curso class updated

--- DesafioDigitalHouse/src/ProfessorAdjunto.kt
+++ DesafioDigitalHouse/src/ProfessorAdjunto.kt
@@ -1,2 +1,20 @@
-class ProfessorAdjunto {
+class ProfessorAdjunto(
+    override var nome: String,
+    override var sobrenome: String,
+    override var codigo: Int,
+    override var tempoDeCasa: Int,
+    var horasDeMonitoria: Int
+) : Professor() {
+
+    override fun equals(other: Any?): Boolean {
+        val outroProfessor = other as? ProfessorAdjunto //talvez precise deixar apenas a Professor ao inves de ProfessorAdjunto
+        return when(other) {
+            is Professor -> {
+                this.codigo == outroProfessor?.codigo
+            }
+            else -> {
+                return super.equals(other)
+            }
+        }
+    }
 }
--- DesafioDigitalHouse/src/ProfessorTitular.kt
+++ DesafioDigitalHouse/src/ProfessorTitular.kt
@@ -2,7 +2,18 @@ class ProfessorTitular(
     override var nome: String,
     override var sobrenome: String,
     override var codigo: Int,
-    override var tempoDeCasa: Int
+    override var tempoDeCasa: Int,
+    var especialidade: String
 ) : Professor() {
-
+    override fun equals(other: Any?): Boolean {
+        val outroProfessor = other as? ProfessorTitular //talvez precise deixar apenas a Professor ao inves de ProfessorTitular
+        return when(other) {
+            is Professor -> {
+                this.codigo == outroProfessor?.codigo
+            }
+            else -> {
+                return super.equals(other)
+            }
+        }
+    }
 }
