c9b8032aeda4f7770f983a2878c6bd37f9058802
nailson13b <nailson.oliveira@outlook.com>
2021-06-05 20:04:32 -0300

    "adicionar and excluir functions included"

--- DesafioDigitalHouse/src/Curso.kt
+++ DesafioDigitalHouse/src/Curso.kt
@@ -19,4 +19,27 @@ class Curso(
     override fun toString(): String  = """
         $nome do código $codigo
     """.trimIndent()
+
+    fun adicionarUmAluno(aluno: Aluno?) {
+        if (listAlunosMatriculados.size < qtMaxAlunos) {
+            if (aluno != null) {
+                listAlunosMatriculados.add(aluno)
+            } else {
+                println("Esperado elemento Aluno")
+            }
+        } else {
+            println("Capacidade máxima de $qtMaxAlunos atingida.")
+        }
+    }
+
+    fun excluirAluno(aluno: Aluno) {
+        for (i in listAlunosMatriculados) {
+            if (i.codigo == aluno.codigo) {
+                listAlunosMatriculados.remove(i)
+                println("Aluno $i removido.")
+                return
+            }
+        }
+        println("Aluno não matriculado.")
+    }
 }
---
+++ DesafioDigitalHouse/src/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+class DigitalHouseManager(
+    var listaAlunos: MutableList<Aluno>,
+    var listaProfessor: MutableList<Professor>,
+    var listaCurso: MutableList<Curso>,
+    var listaMatricula: MutableList<Matricula>
+
+) {
+}
