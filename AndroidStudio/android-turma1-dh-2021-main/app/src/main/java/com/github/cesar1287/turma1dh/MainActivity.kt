2fe63d88356c1d1cce64f77264e373698f1990ab
nailson13b <nailson.oliveira@outlook.com>
2021-06-26 22:46:58 -0300

    fragments

---
+++ AndroidStudio/android-turma1-dh-2021-main/app/src/main/java/com/github/cesar1287/turma1dh/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.github.cesar1287.turma1dh
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("lifecycle", "onCreate")
+
+//        findViewById<Button>(R.id.helloWorld).setOnClickListener {
+//            val intent = Intent(this@MainActivity, SplashActivity::class.java)
+//            intent.putExtra(KEY_INTENT_NOME, "César Rodrigues")
+//            intent.putExtra(KEY_INTENT_SALDO, 50.0)
+//            intent.putExtra(KEY_INTENT_SALDO_LONG, 50L)
+//            startActivity(intent)
+//
+//            //MainActivity -> pacote(Intent) -> SplashActivity
+//        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("lifecycle", "onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("lifecycle", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("lifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("lifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("lifecycle", "onDestroy")
+    }
+
+    companion object {
+        const val KEY_INTENT_NOME = "nome1"
+        const val KEY_INTENT_SALDO = "saldo1"
+        const val KEY_INTENT_SALDO_LONG = "saldoLong1"
+    }
+}
